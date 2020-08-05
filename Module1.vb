466003f880d2c091da4e55277f3b1f7a7b36b4c4
Gloria Zheng <G.Zheng@ampleforth.org.uk>
2020-08-05 20:01:17 +0100

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim star As Integer = 1
+
+        While star < 6
+
+            For n As Integer = 1 To star
+
+                Console.Write("XX")
+
+            Next
+
+            star = star + 1
+
+            Console.WriteLine()
+
+        End While
+
+        Console.Read()
+
+    End Sub
+
+End Module
